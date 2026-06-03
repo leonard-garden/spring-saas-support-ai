bc7ab8058a14225dfb5f327f672d880a98cc339c
Leonard Trinh <trinhvandat90399@gmail.com>
2026-06-03 19:40:32 +0700

    chore(billing): seed stripe_price_id for Starter/Pro/Business plans

---
+++ src/main/resources/db/migration/V23__seed_stripe_price_ids.sql
@@ -0,0 +1,3 @@
+UPDATE plans SET stripe_price_id = 'price_1TeE4kD1cIPP9wqLaLKwz8bj' WHERE slug = 'starter';
+UPDATE plans SET stripe_price_id = 'price_1TeE4lD1cIPP9wqLpkvbAAFs' WHERE slug = 'pro';
+UPDATE plans SET stripe_price_id = 'price_1TeE4nD1cIPP9wqLW232QG7O' WHERE slug = 'business';
